ce6563097aa5d1d83fd7c5e8f36958a4824f5fe6
Yona <1302jona@gmail.com>
2018-05-16 14:42:45 -0500

    xd

--- pages/forms/general.php
+++ pages/forms/general.php
@@ -1,4 +1,3 @@
-
 <?php
 
   
@@ -6,7 +5,7 @@
   session_start();
   if(!isset($_SESSION["user"]))
   {
-   header("location:login/IniciarSesion.html");  //sino inicio seccion lo dirigimos al login
+   header("location:../login/IniciarSesion.html");  //sino inicio seccion lo dirigimos al login
   }
 
 
@@ -19,32 +18,20 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Sistema de citas</title>
+  <title>Sistema de citas-Dar de alta</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
-  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="../../dist/css/AdminLTE.min.css">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="plugins/iCheck/flat/blue.css">
-  <!-- Morris chart -->
-  <link rel="stylesheet" href="plugins/morris/morris.css">
-  <!-- jvectormap -->
-  <link rel="stylesheet" href="plugins/jvectormap/jquery-jvectormap-1.2.2.css">
-  <!-- Date Picker -->
-  <link rel="stylesheet" href="plugins/datepicker/datepicker3.css">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
-  <!-- bootstrap wysihtml5 - text editor -->
-  <link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+  <link rel="stylesheet" href="../../dist/css/skins/_all-skins.min.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -54,6 +41,7 @@
   <![endif]-->
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
+
 <div class="wrapper">
 
   <header class="main-header">
@@ -82,7 +70,7 @@
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="dist/img/logo2.png" class="user-image" alt="User Image">
+              <img src="../dist/img/logo2.png" class="user-image" alt="User Image">
               <span class="hidden-xs">
                 <?php
                 echo' '.$_SESSION["user"];
@@ -93,7 +81,7 @@
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
-                <img src="dist/img/logo2.png" class="img-circle" alt="User Image">
+                <img src="../dist/img/logo2.png" class="img-circle" alt="User Image">
 
                 <p>
                   Alexander Pierce - Web Developer2
@@ -226,92 +214,104 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Sistema de citas
-        <small>Facultad de Ingenieria Química</small>
+        Registro de Alumno
+        
       </h1>
-    
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Forms</a></li>
+        <li class="active">General Elements</li>
+      </ol>
     </section>
 
     <!-- Main content -->
     <section class="content">
-      <!-- Small boxes (Stat box) -->
       <div class="row">
-        
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-green">
-            <div class="inner">
-              <h3></h3>
+        <!-- left column -->
+        <div class="col-md-10">
+          <!-- general form elements -->
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">Todos los campos son obligatorios</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form role="form" method="post" action="RegistroSQL.php">
+              <div class="box-body">
+                 <div class="form-group">
+                  <label for="exampleInputEmail1">id/matricula</label>
+                  <input class="form-control" type="text" name="id" required placeholder="Inserta id/matricula">
 
-              <p>Estadisticas</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-stats-bars"></i>
-            </div>
-            <a href="#" class="small-box-footer">Acceder <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Nombre</label>
+                  <input  class="form-control" type="text" name="Nombre"  required placeholder="Inserta Nombre">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Apellido paterno</label>
+                  <input class="form-control" type="text" name="Paterno"  required placeholder="Inserta Apellido paterno">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Apellido materno</label>
+                  <input class="form-control"type="text" name="Materno" required="hola" placeholder="Inserta Apellido materno">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Email address</label>
+                  <input class="form-control" type="Email" name="Email" required="hola" placeholder="Inserta Email">
+                </div>
+               
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Contraseña</label>
+                  <input  class="form-control" type="password" name="Contrasenia" placeholder="Inserta Contraseña">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Repetir contraseña</label>
+                  <input  class="form-control"  type="password" required="" name="Contrasenia2" placeholder="Repetir contraseña">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Facultad/Centro de investigación</label>
+                  <input class="form-control" type="text"  required="" name="Area" placeholder="Facultad/Centro de inestigacion etc" >
+                </div>
+              
+                
+              </div>
+              <!-- /.box-body -->
+
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary">Registrar</button>
+              </div>
+            </form>
           </div>
-        </div>
-        <!-- ./col -->
-        
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-red">
-            <div class="inner">
-              <h3></h3>
+          <!-- /.box -->
 
-              <p>Otros</p>
+         
+          <!-- /.box -->
+
+          <!-- /.box -->
+
+         
+              <!-- /.row -->
+
+             
+          <!-- /.box -->
+
+        </div>
+        <!--/.col (left) -->
+        <!-- right column -->
+       
+             
+
+                <!-- radio -->
+              
             </div>
-            <div class="icon">
-              <i class="ion ion-pie-graph"></i>
-            </div>
-            <a href="#" class="small-box-footer">Acceder <i class="fa fa-arrow-circle-right"></i></a>
+            <!-- /.box-body -->
           </div>
+          <!-- /.box -->
         </div>
-        <!-- ./col -->
-
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-yellow">
-            <div class="inner">
-              <h3> </h3>
-
-              <p>Agregar alumnos</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-person-add"></i>
-            </div>
-            <a href="#" class="small-box-footer">Acceder <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-yellow">
-            <div class="inner">
-              <h3> </h3>
-
-              <p>Agregar Docente</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-person-add"></i>
-            </div>
-            <a href="#" class="small-box-footer">Acceder <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
+        <!--/.col (right) -->
       </div>
       <!-- /.row -->
-      <!-- Main row -->
-      <div class="row">
-        <!-- Left col -->
-       
-        <!-- /.Left col -->
-        <!-- right col (We are only adding the ID to make the widgets sortable)-->
-       
-        <!-- right col -->
-      </div>
-      <!-- /.row (main row) -->
-
     </section>
     <!-- /.content -->
   </div>
@@ -521,41 +521,14 @@
 <!-- ./wrapper -->
 
 <!-- jQuery 2.2.3 -->
-<script src="plugins/jQuery/jquery-2.2.3.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button);
-</script>
+<script src="../../plugins/jQuery/jquery-2.2.3.min.js"></script>
 <!-- Bootstrap 3.3.6 -->
-<script src="bootstrap/js/bootstrap.min.js"></script>
-<!-- Morris.js charts -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-<script src="plugins/morris/morris.min.js"></script>
-<!-- Sparkline -->
-<script src="plugins/sparkline/jquery.sparkline.min.js"></script>
-<!-- jvectormap -->
-<script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="plugins/knob/jquery.knob.js"></script>
-<!-- daterangepicker -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
-<script src="plugins/daterangepicker/daterangepicker.js"></script>
-<!-- datepicker -->
-<script src="plugins/datepicker/bootstrap-datepicker.js"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-<!-- Slimscroll -->
-<script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<script src="../../bootstrap/js/bootstrap.min.js"></script>
 <!-- FastClick -->
-<script src="plugins/fastclick/fastclick.js"></script>
+<script src="../../plugins/fastclick/fastclick.js"></script>
 <!-- AdminLTE App -->
-<script src="dist/js/app.min.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="dist/js/pages/dashboard.js"></script>
+<script src="../../dist/js/app.min.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
+<script src="../../dist/js/demo.js"></script>
 </body>
 </html>
